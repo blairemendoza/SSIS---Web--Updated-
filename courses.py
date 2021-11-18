158b490fb96b2cca79434805b9fe40d8896a63d5
blairemendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2021-11-18 20:15:04 +0800

    Include courses blueprint

---
+++ courses.py
@@ -0,0 +1,88 @@
+from flask      import Blueprint, render_template, request, flash, url_for, redirect
+from database   import mysql
+
+# Setup blueprint
+courses = Blueprint('courses', __name__, url_prefix='/courses')
+
+# Courses main page
+@courses.route('/')
+def coursesindex():
+     
+     cur = mysql.get_db().cursor()
+     cur.execute('''SELECT courses.course_code, courses.course_name, colleges.college_name, colleges.college_code
+                    FROM courses, colleges
+                    WHERE courses.college_code=colleges.college_code ORDER BY colleges.college_name''')
+     data1 = cur.fetchall()
+     cur.execute('SELECT * FROM colleges')
+     data2 = cur.fetchall()
+     cur.close()
+
+     # courses -> courses list
+     return render_template('courses.html', courses=data1, colleges=data2)
+
+
+# Courses functions
+# Add course
+@courses.route('/add', methods = ['POST'])
+def addcourse():
+    
+    coursecode  = request.form['inputCourseCode']
+    coursename  = request.form['inputCourseName']
+    collegecode = request.form['inputCollegeCode']
+    
+    try:
+        cur = mysql.get_db().cursor()
+        cur.execute('''INSERT INTO courses
+                       VALUES (%s, %s, %s)''', (coursecode, coursename, collegecode))
+        mysql.get_db().commit()
+        
+        flash('Course has been successfully added.', 'success')
+        return redirect(url_for('courses.coursesindex'))
+        
+    except Exception as e:
+        print('Add course error: ' + str(e))
+        
+        flash('Course code already exists. Enter a unique course code.', 'error')
+        return redirect(url_for('courses.coursesindex'))
+    
+# Update course
+@courses.route('/update', methods = ['POST'])
+def updatecourse():
+    
+    coursecode  = request.form['updateCourseCode']
+    coursename  = request.form['updateCourseName']
+    collegecode = request.form['updateCollegeCode']
+    
+    try:
+        cur = mysql.get_db().cursor()
+        cur.execute('''UPDATE courses
+                       SET course_name=%s, college_code=%s
+                       WHERE course_code=%s''', (coursename, collegecode, coursecode))
+        mysql.get_db().commit()
+        
+        flash('Course has been successfully updated.', 'success')
+        return redirect(url_for('courses.coursesindex'))
+        
+    except Exception as e:
+        print('Update course error: ' + str(e))
+        
+        flash('An unexpected error occurred.', 'error')
+        return redirect(url_for('courses.coursesindex'))
+        
+# Remove course
+@courses.route('/remove/<string:course_code>')
+def removecourse(course_code):
+    
+    try:
+        cur = mysql.get_db().cursor()
+        cur.execute('DELETE FROM courses WHERE course_code=%s', (course_code,))
+        mysql.get_db().commit()
+        
+        flash('Course has been successfully removed.', 'success')
+        return redirect(url_for('courses.coursesindex'))
+
+    except Exception as e:
+        print('Remove course error: ' + str(e))
+        
+        flash('An unexpected error occurred.', 'error')
+        return redirect(url_for('courses.coursesindex'))
--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 # CCC181
 
 
-from flask          import Flask, app, redirect
+from flask          import Flask, app, redirect, render_template
 from database       import mysql
+from colleges       import colleges
+from courses        import courses
 
 
 # Server instance
@@ -15,12 +17,29 @@ app.config['MYSQL_DATABASE_USER']       = 'root'
 app.config['MYSQL_DATABASE_PASSWORD']   = 'password'
 app.config['MYSQL_DATABASE_DB']         = 'students'
 app.config['MYSQL_DATABASE_HOST']       = 'localhost'
+app.secret_key                          = 'monkey'
 mysql.init_app(app)
 
 
+app.register_blueprint(colleges)
+app.register_blueprint(courses)
+
+
+# Home page
+# Shows students list
 @app.route('/')
 def home():
-    return 'Hello world'
+    cur = mysql.get_db().cursor()
+    cur.execute('SELECT * FROM students ORDER BY last_name')
+    data1 = cur.fetchall()
+    cur.execute('SELECT course_code, course_name FROM courses')
+    data2 = cur.fetchall()
+    cur.close()
+    
+    # data1 -> students list | data2 -> course dropdown
+    return render_template('home.html', students=data1, courses=data2)
+    
+    
 
 
 if __name__ == '__main__':
